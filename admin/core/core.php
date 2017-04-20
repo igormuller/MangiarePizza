9049cbfb25acb3e0f94d8dbeff1dfbb2822ee0f2
igor.muller <igor.muller@SNFNS0ATICET107.sistemafiesc.org>
2017-04-20 17:11:23 -0300

    Corrigido o htaccess e o Core.

--- admin/core/core.php
+++ admin/core/core.php
@@ -2,8 +2,7 @@
 class Core{
     public function run(){
         
-        $url = explode("index.php", $_SERVER['PHP_SELF']);
-        $url = end($url);
+        $url = (isset($_GET['q']))?"/".$_GET['q']:"";
         $params = array();
         
         if (!empty($url)) {
